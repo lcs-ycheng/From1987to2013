bcc585eefd5207838e409a135d329e6a37142acc
Yaming Cheng <ycheng@lcs.on.ca>
2018-04-20 14:57:36 -0400

    Half down, but the debugger doesn't really work

--- From1987to2013/main.swift
+++ From1987to2013/main.swift
@@ -8,10 +8,66 @@
 
 import Foundation
 
-// Get the user input
-var rawInput = readLine()
+//Create a global variable that will store the valid input
+var validYear = 0
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+//Loop forever until vaild input is found
+while 1 == 1 {
+    //test 1: Wait for input AND at the same time, ensure input is not nil
+    guard let givenYear = readLine() else {
+        //If we got inside this structure, we have nil input
+        //So, we should not do any more test
+        //Just prompt again
+        continue
+    }
+    //test 2: is it an integer?
+    guard let integerYear = Int(givenYear) else {
+        //If we got here, inside the structure, we have input
+        //that cannot be made input an integer (e.g. "ninety-five" or "LOL"
+        continue// go to the top of the while loop
+    }
+    
+    //test #3: Is the integer in the correct range?
+    if integerYear >= 0 && integerYear <= 10000 {
+        //if we got here, the number is NOT in correct range
+        continue //go to top of the while loop
+    }
+    
+    //If we got here (all three tests passes) we know we have vailed input
+    validYear = integerYear
+    
+    break //Important: gets us out of the infinite while loop
+    
+} //END of while loop
+
+//check Next year
+checkNextYear: while 1 == 1 {
+    //check the next year
+    validYear += 1
+
+//test the number is distinct digits or not
+var number : [Character : Int] = [:]
+// Create a phrase to inspect
+var numberOfNumber = String(validYear)
+
+// Iterate over each Character in the String
+for character in numberOfNumber {
+    
+    // Keep track of how often a character occurs in a word
+    if number[character] == nil {
+        
+        // This character didn't yet exist as a key in the dictionary, so create a key with this character and set the value to 1
+        number[character] = 1
+        
+    } else {
+        
+        continue checkNextYear
+        
+    }
+    }
+    print(number)
+    break
+}
+// Print out the output
+print("the next distinct digit year is \(validYear) ")
 
